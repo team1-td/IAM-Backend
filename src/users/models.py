9a10d50cbaa3bae81b6faf8f349ab21c614f858b
Nicolò Vitelli <nicolo.vitelli1@gmail.com>
2021-01-25 15:36:37 +0100

    initial import

---
+++ src/users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from django import forms
+
+# Create your models here.
+class User(models.Model):
+	id			= models.PositiveSmallIntegerField(primary_key=True, null=False)
+	username	= models.CharField(max_length=20, blank=False, null=False)
+	password	= models.CharField(max_length=20, blank=False, null=False) 
+	email		= models.EmailField(max_length=254, blank=False, null=False)
+	first_name	= models.CharField(max_length=20, blank=False, null=False)
+	last_name	= models.CharField(max_length=20, blank=False, null=False)
