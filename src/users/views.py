2ef2e88ea21092619d2af3c52ed956c3549390b0
Nur Abdu <nur.94@hotmail.it>
2021-02-16 22:03:10 +0100

    [ADD] endpoint login and register for api test

--- src/users/views.py
+++ src/users/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from .models import User
+from django.http import JsonResponse
 
 # Create your views here.
-#def login_view():
-#	return render(request, "users/templates/registration/login.html")
+
+
+def login_view(request):
+    response_data = {
+        'id': 1,
+        'name': 'Login'
+    }
+    return JsonResponse(response_data)
+
+
+def register_view(request):
+    response_data = {
+        'id': 2,
+        'name': 'Register'
+    }
+    return JsonResponse(response_data)
+
+# return render(request, "users/templates/registration/login.html")
+
