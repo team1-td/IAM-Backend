b2182a9addfc23e3ccebba94626bf1ab17481409
Nur Abdu <nur.94@hotmail.it>
2021-02-16 23:58:29 +0100

    [FIX] changed import from .views to users.views

--- src/imagerecognition/urls.py
+++ src/imagerecognition/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import login_view, register_view
+from users.views import login_view, register_view
 
 urlpatterns = [
     path('login/', login_view, name="login"),
