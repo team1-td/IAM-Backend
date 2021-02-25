8197123e8b50dd40a8fbd578da6172e47072f28f
amnur <68960254+amnur@users.noreply.github.com>
2021-02-26 00:37:01 +0100

    [FIX] fix ALLOWED_HOSTS removing https://

--- src/imagerecognition/settings.py
+++ src/imagerecognition/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'hh)73ixn)a4)kw8oa45f)l!)-5x9l4qe@id24mvfi04s00n-_)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://pedantic-newton-615678.netlify.app"]
+ALLOWED_HOSTS = ["pedantic-newton-615678.netlify.app"]
 
 
 # Application definition
