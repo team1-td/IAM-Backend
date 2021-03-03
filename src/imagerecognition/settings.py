d65468e0359ddae7f093f07b8f5807a913196fcc
Nur Abdu <nur.94@hotmail.it>
2021-03-03 23:45:58 +0100

    [COMPLETED] logout

--- src/imagerecognition/settings.py
+++ src/imagerecognition/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     # third party apps
     'corsheaders',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
--- src/imagerecognition/urls.py
+++ src/imagerecognition/urls.py
@@ -19,12 +19,14 @@ from users.views import login_view, register_view
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView,)
 
 
+
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('login/', login_view, name="login"),
     path('register/', register_view, name="register"),
     path('admin/', admin.site.urls),
+    path('logout/', LogoutView.as_view(), name='auth_logout'),
 ]
 
 
